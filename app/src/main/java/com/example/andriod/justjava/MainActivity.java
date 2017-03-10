449ad9de3a9ca8e9f32d9a30e7f45b64e27f2756
ovieu <ovie.u.umukoro@gmail.com>
2017-03-10 17:55:56 -0500

    Add Price TextView

--- app/src/main/java/com/example/andriod/justjava/MainActivity.java
+++ app/src/main/java/com/example/andriod/justjava/MainActivity.java
@@ -1,9 +1,10 @@
 package com.example.andriod.justjava;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,7 +18,8 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(1);
+
+        display(30000);
     }
 
     /**
@@ -27,4 +29,14 @@ public class MainActivity extends AppCompatActivity {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
 }
+
+
