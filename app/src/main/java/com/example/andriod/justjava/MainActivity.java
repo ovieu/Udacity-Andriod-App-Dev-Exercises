2efbbd678dcd63c7d5b68b55817a4106fdb240dd
ovieu <ovie.u.umukoro@gmail.com>
2017-04-07 08:03:09 -0400

    Add whipped ceream option

--- app/src/main/java/com/example/andriod/justjava/MainActivity.java
+++ app/src/main/java/com/example/andriod/justjava/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.andriod.justjava;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
+
 import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,8 +23,12 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
+        CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkBox);
+        boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
+        Log.v("MainActivity", "Has whipped cream: " + hasWhippedCream);
+
         int price = calculatePrice(numberOfCoffee);
-        String priceMessage = createOrderSummary(price);
+        String priceMessage = createOrderSummary(price, hasWhippedCream);
         displayMessage(priceMessage);
         //String priceMessage = "Amount Due " + "$" + price + "\n Thank you!";
         //displayMessage(priceMessage);
@@ -39,11 +46,13 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This method creates an order summary
      *@param priceOfOrder is the total price of an order
+     *@param addWhippedCream indicates whether the user wants whipped cream
      *@return order message
      */
-    public String createOrderSummary(int priceOfOrder) {
+    public String createOrderSummary(int priceOfOrder, boolean addWhippedCream) {
         String orderMessage = "Name: Kaptain Kunal " + "\n"
                             + "Quantity: " + numberOfCoffee + "\n"
+                            + "Add Whipped Cream? " + addWhippedCream + "\n"
                             + "Total: $" + priceOfOrder + "\n"
                             + "Thank You!";
         return orderMessage;
